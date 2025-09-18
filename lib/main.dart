3bddd874ede7d38ac1722658a0e948cfa3cd0cd8
RK <ranziekirth@gmail.com>
2025-09-18 17:42:56 +0800

    Flutter Bottom Navigation

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/navigation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Portfolio App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainNavigationPage(),
+    );
+  }
+}
